8f358cea9f60c5086a46ae5992ad59571b7f41cf
Piotr Kowalski <piotr.kowalski@ts.fujitsu.com>
2016-12-10 14:06:09 +0100

    Last refactoring

--- DemoTT/ViewModel/ViewModelLocator.cs
+++ DemoTT/ViewModel/ViewModelLocator.cs
@@ -28,16 +28,7 @@ namespace DemoTT.ViewModel
         static ViewModelLocator()
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
-
-            if (ViewModelBase.IsInDesignModeStatic)
-            {
-                SimpleIoc.Default.Register<IDataService, Design.DesignDataService>();
-            }
-            else
-            {
-                SimpleIoc.Default.Register<IDataService, DataService>();
-            }
-            
+                        
             SimpleIoc.Default.Register<MainViewModel>();
         }
 
